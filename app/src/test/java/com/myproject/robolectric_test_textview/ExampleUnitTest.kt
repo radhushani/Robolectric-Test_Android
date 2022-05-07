1242cdc7502e06ad99d9d8992010b858841a48ca
Radhushani <radushani111@gmail.com>
2022-05-07 13:01:43 +0530

    Robolectric test for fragment textview

--- app/src/test/java/com/myproject/robolectric_test_textview/ExampleUnitTest.kt
+++ app/src/test/java/com/myproject/robolectric_test_textview/ExampleUnitTest.kt
@@ -2,6 +2,8 @@ package com.myproject.robolectric_test_textview
 
 
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,11 +14,30 @@ import org.robolectric.RobolectricTestRunner
 class ExampleUnitTest {
 
     @Test
-    fun check_main_activity_text() {
+    fun check_second_activity_text() {
         val activity = Robolectric.buildActivity(SecondActivity::class.java).create().get()
         val textView : TextView = activity.findViewById<TextView>(R.id.textViewActivity)
         assertEquals("Hello World!!", textView.text)
     }
+
+    @Test
+    fun check_first_fragment_text(){
+        val fragment = FirstFragment()
+        val activity = Robolectric.buildActivity(
+            MainActivity::class.java
+        )
+            .create()
+            .start()
+            .resume()
+            .get()
+        val fragmentManager: FragmentManager = activity.supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(fragment, null)
+        fragmentTransaction.commit()
+        val textView : TextView = activity.findViewById<TextView>(R.id.textViewFragment)
+        assertEquals("Hello world!!", textView.text)
+    }
+
 
 
 }
